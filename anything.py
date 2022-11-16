4cd6b8726cbb105e8e1ec6218ac27b97c8e675ac
20174318HojunKim <rlaghwns711@naver.com>
2022-11-16 23:01:08 +0900

    edit anything.py

---
+++ anything.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    print("hello python, hi ros.")
+    print("Python is the best language for ros")
+
+~/workspace/robotics-schl$ git status
+~/workspace/robotics-schl$ git diff
+~/workspace/robotics-schl$ git add .
+~/workspace/robotics-schl$ git commit -m 'edit anything.py'
+
